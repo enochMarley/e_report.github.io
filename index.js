21ed2c4421544ba881df82bcbb23ea238667ebc1
Enoch Sowah <enoch.sowah16@gmail.com>
2018-05-20 21:16:37 +0000

    another commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express'); //import the express module
+var app = express(); //init express
+var server =  require('http').createServer(app); //create server
+var path = require('path'); //init the path module
+var port = process.env.PORT || 5000; //init the port number
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//use static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//listen on server
+server.listen(port);
+
+//initializing session variable
+var appSession;
+
+//when the index page is requested
+app.get('/', (req,res) => {
+	res.render('index',{title:'E-report'});
+});
